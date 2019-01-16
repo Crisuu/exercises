c837a8196ebf3b3458be1ee2b01a2aed1a920efb
Crisuu <Kwroblewski314@gmail.com>
2019-01-16 14:13:33 +0100

    first exercise

---
+++ zmianaKolejnosci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odwrocNapis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz słowo:");
+            string slowo = Console.ReadLine();
+            Console.WriteLine(odwrocNapis(slowo, 'a'));
+            Console.ReadKey();
+        }
+        static string odwrocNapis(string napis, char wyklucz)
+        {
+            StringBuilder sb = new StringBuilder();
+
+                    for (int i = napis.Length - 1; i >= 0; i--)
+                    {
+                        sb.Append(napis[i]);
+                    }
+            return sb.ToString();
+        }
+    }
+    
+}
